c4ca43d79e156908afe838fe70bee28f8797c3eb
City <city@nanzc.com>
2021-04-05 19:57:10 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>App</div>
+    <Router>
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
