caf931d53150b948080c9fc9be780cadc56bdc44
City <city@nanzc.com>
2021-08-24 18:21:48 +0800

    feat: add lib

--- src/libs/javascript/Object.ts
+++ src/libs/javascript/Object.ts
@@ -1,9 +1,7 @@
-import { IAllProps } from '@typings/public/types'
-
 /**
  * 删除对象元素
  */
-export const deleteObject = <T extends IAllProps>(key: string, obj: T): T => {
+export const deleteObject = <T extends { [k: string]: any }>(key: string, obj: T): T => {
   if (obj[key]) delete obj[key]
 
   return { ...obj }
